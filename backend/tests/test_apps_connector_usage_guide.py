14f3754cad0a032b2503490a3993bc8336b181ac
Vincent <vincent.bannister@gmail.com>
2026-05-05 00:32:16 -0400

    Revert "Require app SDK triggerWorkflow for app workflow triggers"

--- backend/tests/test_apps_connector_usage_guide.py
+++
@@ -1,7 +0,0 @@
-from connectors.apps import USAGE_GUIDE
-
-
-def test_usage_guide_requires_sdk_trigger_workflow_helper() -> None:
-    assert "Always call the SDK helper: `triggerWorkflow(...)`" in USAGE_GUIDE
-    assert "Do NOT hand-roll `window.parent.postMessage(...)`" in USAGE_GUIDE
-    assert "app-trigger-workflow" in USAGE_GUIDE
